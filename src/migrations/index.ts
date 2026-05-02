8f49464d5dcdd55e57b3316c21539e54ee441ce3
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-05-02 23:24:59 +0200

    Fix migrations index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,15 @@
-import type { Migration } from '@/types/migration-task.types';
+import { MigrationTask } from '@/types/migration-task.types';
 import migration0001 from './0001_add_last_refresh_failed_to_blocklists';
 
-export const migrations: Migration[] = [
+export const migrations: MigrationTask[] = [
   {
     ...migration0001,
     timestamp: Date.now(),
   },
 ];
 
-export const getMigrations = (): Migration[] => migrations;
+export const getMigrations = (): MigrationTask[] => migrations;
 
-export const getPendingMigrations = (applied: string[]): Migration[] => {
+export const getPendingMigrations = (applied: string[]): MigrationTask[] => {
   return migrations.filter((m) => !applied.includes(m.name));
 };
