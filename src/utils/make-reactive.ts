172e134ea024d093a1947e4a6e9acdcdb746d88b
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-06-01 00:30:17 +0200

    Revert "Fix blocklist refresh status object"
    This reverts commit 28e4f24798ba7db9f95a30fa3e8efea9dfac8833.
    
    # Conflicts:
    #	src/services/blocklists/status-blocklist.service.ts

--- src/utils/make-reactive.ts
+++ src/utils/make-reactive.ts
@@ -1,14 +1,5 @@
-const proxyCache = new WeakMap<object, object>();
-
 export function makeReactive<T extends object>(obj: T, onChange: () => void): T {
-  // Return cached proxy if one already exists for this object
-  // This ensures every underlying object has exactly one stable proxy,
-  // preventing reference aliasing during nested access and JSON serialization.
-  if (proxyCache.has(obj)) {
-    return proxyCache.get(obj) as T;
-  }
-
-  const proxy = new Proxy(obj, {
+  return new Proxy(obj, {
     get(target, prop, receiver) {
       const value = Reflect.get(target, prop, receiver);
       if (value !== null && typeof value === 'object') {
@@ -25,7 +16,4 @@ export function makeReactive<T extends object>(obj: T, onChange: () => void): T 
       return result;
     },
   }) as T;
-
-  proxyCache.set(obj, proxy);
-  return proxy;
 }
